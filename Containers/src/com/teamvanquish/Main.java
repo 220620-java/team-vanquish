07ca3fa1fdc744a906e20fc9b5998e3ed1194ab1
New Pagodi <NewPagodi@users.noreply.github.com>
2022-06-23 12:46:44 -0500

    Initial implimentation for comparison

--- Containers/src/com/teamvanquish/Main.java
+++ Containers/src/com/teamvanquish/Main.java
@@ -1,5 +1,58 @@
 package com.teamvanquish;
 
-public class Main {
+import com.teamvanquish.ArrayList;
 
+class TempObject
+{
+    static {
+        maxId = 0;
+    }
+    
+    TempObject() {
+        id = maxId;
+        maxId++;
+    }
+    
+    
+    public void Display()
+    {
+        System.out.println("This is a temp object " + id + ".");
+    }
+    
+    int id;
+    static int maxId;
+}
+
+public class Main {
+    
+
+    public static void main(String[] args) {
+        
+        ArrayList myList = new ArrayList();
+        
+        myList.add(new TempObject());
+        myList.add(new TempObject());
+        myList.add(new TempObject());
+        
+        Object o = myList.get(2);
+        
+        if ( o == null )
+        {
+            System.out.println("Failed to add to the ArrayList");
+        }
+        else
+        {
+            ((TempObject) o).Display();
+        }
+        
+
+
+    }
+    
+
+    //myList.add(new TempObject());
+    
+    
+    
+    
 }
