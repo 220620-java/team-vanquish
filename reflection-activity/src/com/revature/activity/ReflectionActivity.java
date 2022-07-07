cfd77384abcb8b52a267bdc84318f04135cb2470
evgeniyko99 <ekk4357@gmail.com>
2022-07-07 17:01:34 -0400

    reflection activity

--- reflection-activity/src/com/revature/activity/ReflectionActivity.java
+++ reflection-activity/src/com/revature/activity/ReflectionActivity.java
@@ -1,27 +1,70 @@
 package com.revature.activity;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 import com.revature.exercise.SecretClass;
 
 public class ReflectionActivity {
 
 	/*
 	 * Find all of the following:
-	 * 1. What fields does the secret class have?
-	 * 2. What methods does the secret class have?
-	 * 3. What are the modifiers on the fields and the methods?
-	 * 4. What happens when you call each of the methods?
-	 * 5. What are the values of the fields?
+	 * 1. What fields does the secret class have? id, name, message, passcode
+	 * 2. What methods does the secret class have? getMessage getStaticMessahe
+	 * 3. What are the modifiers on the fields and the methods? private, private, private, private final, public, public static
+	 * 4. What happens when you call each of the methods? https://i.ytimg.com/vi/GUlWIxrZp9M/maxresdefault.jpg https://www.youtube.com/watch?v=dQw4w9WgXcQ
+	 * 5. What are the values of the fields? 4, Alchemy, https://i.ytimg.com/vi/GUlWIxrZp9M/maxresdefault.jpg, 123123321
 	 * 6. What annotations are used in the secret class?
-	 * 7. What fields do the annotations have?
-	 * 8. Which fields in the secret class use the annotations?
-	 * 9. What are the values of those annotations' fields on each class field?
+	 * 7. What fields do the annotations have? 
+	 * 8. Which fields in the secret class use the annotations? 
+	 * 9. What are the values of those annotations' fields on each class field? 
 	 * 10. What annotation(s) does the class itself have, and what are the values of 
 	 *     the field(s) on them?
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		SecretClass secret = new SecretClass();
+		Class objClass = secret.getClass();
+		Field[] fields = objClass.getDeclaredFields();
+		for (Field field : fields) {
+			System.out.println("Fields are: " + field.getName());
+			int modifier = field.getModifiers();
+			System.out.println("Modifiers are: " + Modifier.toString(modifier));
+			Object type = field.getType();
+			field.setAccessible(true);
+			Object value = field.get(secret);
+			field.set(secret, value);
+			System.out.println("Value is: " + field.get(secret));
+			System.out.println(field.getName() + "'s annotations are: ");
+			Annotation[] annotations = field.getDeclaredAnnotations();
+			System.out.println(annotations.length);
+			for (Annotation annotation : annotations) {
+				System.out.println(annotation);
+			}
+			System.out.println();
+		}
+		Method[] methods = objClass.getDeclaredMethods();
+		for (Method method : methods) {
+			System.out.println("Methods are: " + method.getName());
+			int modifier = method.getModifiers();
+			System.out.println("Modifiers are: " + Modifier.toString(modifier));
+			System.out.println(method.getName() + "'s annotations are: ");
+			Annotation[] annotations = method.getDeclaredAnnotations();
+			for (Annotation annotation : annotations) {
+				System.out.println(annotation);
+			}
+			System.out.println();
+		}
+		System.out.println(secret.getMessage());
+		System.out.println(secret.getStaticMessage());
+		System.out.println();
 		
-		
+		Annotation[] declaredAnnotations = secret.getClass().getDeclaredAnnotations();
+		System.out.println("Class annotations are: ");
+		for (Annotation annotation : declaredAnnotations) {
+			System.out.println(annotation);
+		}
 	}
 
 	/*
